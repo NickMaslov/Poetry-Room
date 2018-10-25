32bc5d10924e18ab7b54229da2f32878d80160d8
Mykola Maslov <kategorychnost@gmail.com>
2018-10-25 16:21:59 -0400

    preparation for deployment

--- client/src/index.js
+++ client/src/index.js
@@ -22,7 +22,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4444/graphql',
+  uri: 'https://recipes--book.herokuapp.com/graphql',
   fetchOptions: {
     credentials: 'include',
   },
